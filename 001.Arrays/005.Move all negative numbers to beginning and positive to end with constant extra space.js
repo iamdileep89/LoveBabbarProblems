b0c5813a092dd0c3bc3043f40e2f0a44ddd61073
Dileep <98578797+iamdileep89@users.noreply.github.com>
2022-04-14 20:00:57 +0000

    more practice

---
+++ 001.Arrays/005.Move all negative numbers to beginning and positive to end with constant extra space.js
@@ -0,0 +1,49 @@
+
+// Appraoch 1 - Quick sort methodology
+function rearrange(arr){
+    let j=0
+    for(let i =0; i<arr.length; i++){
+        if(arr[i]<0){
+            if(i!=j){
+                let temp = arr[i]
+                arr[i] = arr[j]
+                arr[j] = temp
+            }
+            j++
+        }
+    }
+
+   return arr
+}
+
+rearrange([ -1, 2, -3, 4, 5, 6, -7, 8, 9 ]) //[-1, -7, -3, 4, 5, 6, 2, 8, 9]
+
+
+// time complexity - O(n)
+// space complexity - O(1)
+
+// Approach -2 --- Two pointer pattern
+
+function rearrange1(arr){
+    let left = 0
+    let right = arr.length-1
+
+    while(left<=right){
+        if(arr[left]<0 && arr[right] < 0){
+            left++
+        } else if(arr[left] >0 && arr[right]<0){
+            let temp = arr[left]
+            arr[left] = arr[right]
+            arr[right] = temp
+            left++
+            right--
+        } else if(arr[left] > 0 && arr[right] > 0){
+            right--
+        } else {
+            left++
+            right--
+        }
+    } 
+    return arr
+}
+rearrange1([ -1, 2, -3, 4, 5, 6, -7, 8, 9 ]) //[-1, -7, -3, 4, 5, 6, 2, 8, 9]
